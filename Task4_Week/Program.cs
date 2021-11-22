ec4a85dedeaee20916d7039991e5adc1382f18d4
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-22 11:31:46 +0300

    Task 4 resolved

---
+++ Task4_Week/Program.cs
@@ -0,0 +1,13 @@
+﻿string [] array = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine("Enter day number");
+
+int DayOfWeek = int.Parse(Console.ReadLine());
+
+if (DayOfWeek > 7 || DayOfWeek < 1)
+{
+Console.WriteLine("There are 7 days a week");
+}
+else
+{
+Console.WriteLine(array [DayOfWeek-1]);
+}
