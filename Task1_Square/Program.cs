2e546eb01c2af227b0adb72bed55e8af04b5998f
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-22 10:50:44 +0300

    Min_max task resolved

---
+++ Task1_Square/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Enter your number");
+int Number = int.Parse(Console.ReadLine());
+Console.WriteLine("Square = {0}", Number*Number);
+
---
+++ Task3_Max_min/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number 1");
+int NumberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA>NumberB)
+{
+    Console.WriteLine("Max number is - {0}", NumberA);
+    Console.WriteLine("Min number is  - {0}", NumberB);
+}
+else 
+{
+    Console.WriteLine("Max number is  - {0}", NumberB);
+    Console.WriteLine("Min number is - {0}", NumberA);
+}
