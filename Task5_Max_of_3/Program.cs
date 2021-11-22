b8bd2a28152618593b5944355a61c05886a13ace
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-22 11:48:39 +0300

    Task 5 completed

---
+++ Task5_Max_of_3/Program.cs
@@ -0,0 +1,11 @@
+﻿int Number1 = new Random().Next(1,11);
+Console.WriteLine(Number1);
+int Number2 = new Random().Next(1,11);
+Console.WriteLine(Number2);
+int Number3 = new Random().Next(1,11);
+Console.WriteLine(Number3);
+int max = Number1;
+if (Number1 > max) max = Number1;
+if (Number2 > max) max = Number2;
+if (Number3 > max) max = Number3;
+Console.WriteLine("Max number is - {0}", max);
