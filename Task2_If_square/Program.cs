89147c156dde27f353bf21c2317b3813dc40e43e
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-22 11:55:33 +0300

    Task 2 resolved

---
+++ Task2_If_square/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter number 1");
+int NumberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter number 2");
+int NumberB = int.Parse(Console.ReadLine());
+if (NumberA*NumberA == NumberB)
+{
+Console.WriteLine("Yes, number 2 is a square of number 1");
+}
+else 
+{
+    Console.WriteLine("No, number 2 is NOT a square of number 1");
+}
