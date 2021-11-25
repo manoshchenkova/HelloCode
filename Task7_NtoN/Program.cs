9c2fcaef5081618b06cdade6ca02a7d54f7bcb53
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-25 11:07:51 +0300

    Resolved tasks 5, 6 and 7

---
+++ Task7_NtoN/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать числа от -N до N
+
+int PositiveNumber = new Random().Next(1,10);
+Console.WriteLine(PositiveNumber);
+
+int NegativeNumber = -PositiveNumber;
+
+while(NegativeNumber <= PositiveNumber)
+{
+        Console.WriteLine(NegativeNumber);
+        NegativeNumber = NegativeNumber + 1;
+}
+
