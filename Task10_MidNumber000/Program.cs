96ecc2b332b8a3614a970e3d4a268718fc821262
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-25 11:56:14 +0300

    Task 10 resolved

---
+++ Task10_MidNumber000/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+int N = new Random().Next(100,999);
+Console.WriteLine("Our number is - {0}", N);
+
+N = N%100;
+N = (N/10);
+
+Console.WriteLine("Middle number is {0}", N);
