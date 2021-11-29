7be1d577d1afaf67b06d04fa2e767095da75ee53
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-29 11:58:31 +0300

    Task 16 resolved

---
+++ Task16DayOfWeek/Program.cs
@@ -0,0 +1,19 @@
+﻿//16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+string [] array = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+Console.WriteLine("Enter day number");
+
+int DayOfWeek = int.Parse(Console.ReadLine());
+
+if (DayOfWeek < 1 || DayOfWeek > 7)
+    {
+        Console.WriteLine("There are 7 days a week");
+    }
+    if (DayOfWeek == 6 || DayOfWeek == 7)
+        {
+             Console.WriteLine($"{array[DayOfWeek-1]} is a weekend");
+        }
+    else
+    {
+        Console.WriteLine($"{array[DayOfWeek-1]} is NOT a weekend");
+    }
