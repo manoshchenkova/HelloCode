72316971ad3ba1cae0bc6ecf5e44a1b288a059e3
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-29 11:38:05 +0300

    Task 15 resolved, task 14 fixed

---
+++ Task14_3rdDidgit/Program.cs
@@ -0,0 +1,18 @@
+﻿//14. Найти третью цифру числа или сообщить, что её нет
+
+Console.Write("Our number is ");
+ int N = new Random().Next(1, 99999999);
+ Console.WriteLine(N);
+
+ if (N>99)
+     {
+      while (N>1000)
+      {
+          N = N / 10;
+      }
+      Console.WriteLine($"Third digit is {N%10}");
+     }
+else 
+ {
+     Console.WriteLine("There's no 3rd digit in this number");
+ }
