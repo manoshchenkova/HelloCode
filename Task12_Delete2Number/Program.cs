df113fdefec656b71c88fa5ace6e3256486c7b77
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-11-29 11:14:37 +0300

    Task 15 resolved

---
+++ Task12_Delete2Number/Program.cs
@@ -0,0 +1,11 @@
+﻿//12. Удалить вторую цифру трёхзначного числа
+Console.Write("Our number is ");
+ int N = new Random().Next(100, 999);
+ Console.WriteLine(N);
+
+ int N1 = N/100;
+ int N3 = N%10;
+
+Console.Write(N1);
+Console.Write(N3);
+
---
+++ Task15_7_23/Program.cs
@@ -0,0 +1,18 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.Write("Our number is ");
+ int Number = new Random().Next(1, 999);
+ //int Number = 161;
+ Console.WriteLine(Number);
+
+int FirstDivider = 7;
+int SecondDivider = 23;
+
+if (Number%FirstDivider == 0 && Number%SecondDivider == 0)
+{
+    Console.WriteLine($"Число {Number} кратно {FirstDivider} и {SecondDivider}");
+}
+else 
+{
+    Console.WriteLine($"Число {Number} НЕ кратно {FirstDivider} и {SecondDivider}");
+}
