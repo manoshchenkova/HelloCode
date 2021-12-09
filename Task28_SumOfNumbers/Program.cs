f07b51b5bcbb055a9dcd436ca71e11f6d2eb2b02
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-09 11:18:01 +0300

    Task 28 resolved

---
+++ Task28_SumOfNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿//28. Подсчитать сумму цифр в числе
+
+Console.Clear();
+int Number = new Random().Next(1,1000);
+Console.WriteLine($"Our number is {Number}");
+
+int Sum = 0 ;
+int ShortenedNumber = Number;
+
+while (Number > 0)
+{
+    ShortenedNumber = Number % 10;
+    Sum += ShortenedNumber;
+    Number = Number/10;
+}
+Console.WriteLine($"Sum of digits is {Sum}");
