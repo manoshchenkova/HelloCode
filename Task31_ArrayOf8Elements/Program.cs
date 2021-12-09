f0a4d3cf281a0b16aea9e4d951913c34f3c0da11
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-09 11:54:33 +0300

    Task 31 resolved

---
+++ Task31_ArrayOf8Elements/Program.cs
@@ -0,0 +1,29 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+Console.Clear();
+
+int[] Array = new int[8];
+
+void FillArray(int[] Array1)
+{
+    int i = 0;
+    while (i < Array1.Length)
+    {
+        Array1[i] = new Random().Next(1,10);
+        i++;
+    }
+}
+
+void PrintArray(int[] Array2)
+{
+    int j = 0;
+    while(j<Array2.Length)
+        {
+            Console.Write($" {Array2[j]} ");
+            j++;
+        }
+}
+PrintArray(Array);
+Console.WriteLine();
+FillArray(Array);
+PrintArray(Array);
