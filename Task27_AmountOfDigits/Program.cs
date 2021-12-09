b1784b6c1c0d33191fc414eea9efb607d9a011ba
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-09 11:00:26 +0300

    Task 27 resolved

---
+++ Task27_AmountOfDigits/Program.cs
@@ -0,0 +1,14 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.Clear();
+double Number = new Random().Next(1,10000000);
+Console.WriteLine($"Our number is {Number}");
+
+int count = 0;
+
+while (Number > 10)
+{
+    Number = Number/10;
+    count++;
+}
+Console.WriteLine($"Amount of digits is {count+1}");
