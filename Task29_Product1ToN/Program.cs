e73abb4914b519f35cb4a68ca91601f50cea3624
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-09 11:26:21 +0300

    Task 29 resolved

---
+++ Task29_Product1ToN/Program.cs
@@ -0,0 +1,13 @@
+﻿//29. Написать программу вычисления произведения чисел от 1 до N
+
+Console.Clear();
+int N = new Random().Next(1,10);
+Console.WriteLine($"Our number is {N}");
+
+double Product = 1;
+
+for (int i = 1; i <= N; i++)
+{
+    Product *= i;
+}
+Console.WriteLine($"Product of digits is {Product}");
