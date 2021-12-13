439765e57872b4faf79c91d3fe2a281dc55fa43b
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-13 11:43:14 +0300

    Task 49 resolved

---
+++ Task49_DoubleArray_Fractual/Program.cs
@@ -0,0 +1,37 @@
+﻿// 49. Показать двумерный массив размером m×n заполненный вещественными числами
+
+Console.Clear();
+
+Console.WriteLine("Please enter number of rows");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter number of columns");
+int columns = int.Parse(Console.ReadLine());
+
+double[,] doubleArray = new double[rows, columns];
+
+void FillDoubleArray(double[,] DoubleArray)
+{
+    for(int i = 0; i < DoubleArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < DoubleArray.GetLength(1); j++)
+        {
+            DoubleArray[i,j]= new Random().Next(1,10) + new Random().NextDouble();
+        }
+    }
+}
+
+void PrintDoubleArray(double[,] DoubleArray)
+{
+    for(int i = 0; i < DoubleArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < DoubleArray.GetLength(1); j++)
+        {
+            Console.Write(DoubleArray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillDoubleArray(doubleArray);
+PrintDoubleArray(doubleArray);
