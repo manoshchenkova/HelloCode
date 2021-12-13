a8c56f748cb2211b7f92760a43ac5c50a2f3cc81
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-13 12:02:36 +0300

    Task 51 resolved

---
+++ Task51_m+n/Program.cs
@@ -0,0 +1,37 @@
+﻿// 51. Задать двумерный массив следующим правилом: Aₘₙ = m+n
+
+Console.Clear();
+
+Console.WriteLine("Please enter number of rows");
+int rows = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter number of columns");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] doubleArray = new int[rows, columns];
+
+void FillDoubleArray(int[,] DoubleArray)
+{
+    for(int i = 0; i < DoubleArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < DoubleArray.GetLength(1); j++)
+        {
+            DoubleArray[i,j]= i + j;
+        }
+    }
+}
+
+void PrintDoubleArray(int[,] DoubleArray)
+{
+    for(int i = 0; i < DoubleArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < DoubleArray.GetLength(1); j++)
+        {
+            Console.Write(DoubleArray[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+FillDoubleArray(doubleArray);
+PrintDoubleArray(doubleArray);
