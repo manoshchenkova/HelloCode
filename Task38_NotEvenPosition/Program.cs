195f179fd2e52115387c1880e8f11e738d123900
Dmitry Anoschenkov <danoschenkov@gmail.com>
2021-12-16 09:51:11 +0300

    Task 38 resolved

---
+++ Task38_NotEvenPosition/Program.cs
@@ -0,0 +1,29 @@
+﻿// 38. Найти сумму чисел одномерного массива стоящих на нечетной позиции
+
+void FillArray(int[] Array1)
+{
+    for(int i = 0; i < Array1.Length; i++)
+    {
+        Array1[i] = new Random().Next(1,10);
+        Console.Write(Array1[i] + " ");
+    }
+}
+
+void SumOfOddIndexes(int[] Array2)
+{
+    int Sum = 0;
+    for(int j = 0; j < Array2.Length; j++)
+    {
+        if (j %2 == 1)
+        Sum = Sum + Array2[j];
+    }
+    Console.WriteLine($"The sum of numbers in odd indexes in this array is {Sum}");
+}
+
+Console.Clear();
+
+int[] Array = new int[10];
+
+FillArray(Array);
+Console.WriteLine();
+SumOfOddIndexes(Array);
